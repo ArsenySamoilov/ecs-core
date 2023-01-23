a96531e8066a65a02d3c102f131a832de1bf77b3
ArsenySamoilov <samoilov.arseny@gmail.com>
2023-01-23 23:58:27 +0300

    Add Entities and Boxed Entities

---
+++ Runtime/BoxedEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace SemsamECS.Core
+{
+    public readonly struct BoxedEntity
+    {
+        public readonly int Id;
+        public readonly int Gen;
+
+        public BoxedEntity(int id, int gen)
+        {
+            Id = id;
+            Gen = gen;
+        }
+    }
+}
---
+++ Runtime/Entities.cs
@@ -0,0 +1,61 @@
+﻿namespace SemsamECS.Core
+{
+    /// <summary>
+    /// Container for entities, can create and remove entities and check their existence.
+    /// </summary>
+    public sealed class Entities
+    {
+        private readonly int[] _entities;
+        private readonly int[] _entitiesGen;
+        private int _nextEntity;
+
+        public Entities(int maxEntitiesAmount)
+        {
+            _entities = new int[maxEntitiesAmount];
+            _entitiesGen = new int[maxEntitiesAmount];
+            _nextEntity = 0;
+            for (var i = 1; i < maxEntitiesAmount; ++i)
+                _entities[i - 1] = i;
+        }
+
+        /// <summary>
+        /// Creates an entity.
+        /// </summary>
+        public int Create()
+        {
+            var entity = _nextEntity;
+            _nextEntity = _entities[entity];
+            _entities[entity] = entity;
+            return entity;
+        }
+
+        /// <summary>
+        /// Removes the entity.
+        /// </summary>
+        public void Remove(int entity)
+        {
+            _entities[entity] = _nextEntity;
+            _nextEntity = entity;
+            ++_entitiesGen[entity];
+        }
+
+        /// <summary>
+        /// Boxes the entity for safe storing.
+        /// </summary>
+        public BoxedEntity Box(int entity)
+        {
+            return new BoxedEntity(entity, _entitiesGen[entity]);
+        }
+        
+        /// <summary>
+        /// Tries to unbox the boxed entity.
+        /// </summary>
+        /// <returns>True if unboxed successfully, false elsewhere.</returns>
+        public bool TryUnbox(BoxedEntity boxedEntity, out int entity)
+        {
+            var boxedEntityId = boxedEntity.Id;
+            entity = boxedEntityId;
+            return _entities[boxedEntityId] == boxedEntityId && _entitiesGen[boxedEntityId] == boxedEntity.Gen;
+        }
+    }
+}
