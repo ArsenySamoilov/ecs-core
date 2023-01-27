3e7c30ab62151d8e730b7f4ebf99e329205856fb
ArsenySamoilov <samoilov.arseny@gmail.com>
2023-01-27 21:34:00 +0300

    Add Pooled World

---
+++ Runtime/Worlds/PooledWorld.cs
@@ -0,0 +1,18 @@
+﻿namespace SemsamECS.Core
+{
+    public sealed class PooledWorld
+    {
+        public readonly Entities Entities;
+        public readonly Pools Pools;
+        public readonly Groups Groups;
+        public readonly Systems Systems;
+
+        public PooledWorld(int numberMaxEntities, int numberMaxComponents, int numberMaxGrouped)
+        {
+            Entities = new Entities(numberMaxEntities);
+            Pools = new Pools(numberMaxEntities, numberMaxComponents);
+            Groups = new Groups(Pools, numberMaxEntities, numberMaxGrouped);
+            Systems = new Systems();
+        }
+    }
+}
