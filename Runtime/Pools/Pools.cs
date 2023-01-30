1a2f93f2ad693cbac73b78fd80a5def65953885b
ArsenySamoilov <samoilov.arseny@gmail.com>
2023-01-30 23:39:44 +0300

    Fix Pools
    Getting of Pool might be not possible due to using TagPool. Three different methods for returning IPool, Pool and TagPool are created.

--- Runtime/Pools/Pools.cs
+++ Runtime/Pools/Pools.cs
@@ -41,16 +41,40 @@
         }
 
         /// <summary>
-        /// Returns the pool of type <typeparamref name="TComponent"/> and creates it if needed.
+        /// Returns the interface of pool of type <typeparamref name="TComponent"/> and creates it if needed.
         /// </summary>
         /// <param name="numberMaxComponents">Specified components' capacity for the pool if it needs to be created.</param>
         /// <param name="isTag">Is <typeparamref name="TComponent"/> a tag</param>
-        public Pool<TComponent> Get<TComponent>(int numberMaxComponents = 0, bool isTag = false) where TComponent : struct
+        public IPool Get<TComponent>(int numberMaxComponents = 0, bool isTag = false) where TComponent : struct
+        {
+            for (var i = 0; i < _poolCount; ++i)
+                if (typeof(TComponent) == _pools[i].GetComponentType())
+                    return _pools[i];
+            return Create<TComponent>(numberMaxComponents, isTag);
+        }
+
+        /// <summary>
+        /// Returns the pool of type <typeparamref name="TComponent"/> and creates it if needed.
+        /// </summary>
+        /// <param name="numberMaxComponents">Specified components' capacity for the pool if it needs to be created.</param>
+        public Pool<TComponent> GetPool<TComponent>(int numberMaxComponents = 0) where TComponent : struct
         {
             for (var i = 0; i < _poolCount; ++i)
                 if (typeof(TComponent) == _pools[i].GetComponentType())
                     return (Pool<TComponent>)_pools[i];
-            return (Pool<TComponent>)Create<TComponent>(numberMaxComponents, isTag);
+            return (Pool<TComponent>)Create<TComponent>(numberMaxComponents, false);
+        }
+
+        /// <summary>
+        /// Returns the tag pool of type <typeparamref name="TComponent"/> and creates it if needed.
+        /// </summary>
+        /// <param name="numberMaxComponents">Specified components' capacity for the pool if it needs to be created.</param>
+        public TagPool<TComponent> GetTagPool<TComponent>(int numberMaxComponents = 0) where TComponent : struct
+        {
+            for (var i = 0; i < _poolCount; ++i)
+                if (typeof(TComponent) == _pools[i].GetComponentType())
+                    return (TagPool<TComponent>)_pools[i];
+            return (TagPool<TComponent>)Create<TComponent>(numberMaxComponents, true);
         }
 
         private IPool Create<TComponent>(int numberMaxComponents, bool isTag) where TComponent : struct
