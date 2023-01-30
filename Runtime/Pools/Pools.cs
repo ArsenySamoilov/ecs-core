699c12d79e0e773943d1b49bcb6e40865d951070
ArsenySamoilov <samoilov.arseny@gmail.com>
2023-01-30 21:54:20 +0300

    Improve Pools' API
    Adding pool to Pools is revealed. IsTag parameter's information is added to the documentation comments.

--- Runtime/Pools/Pools.cs
+++ Runtime/Pools/Pools.cs
@@ -17,26 +17,37 @@
             _pools = System.Array.Empty<IPool>();
             _poolCount = 0;
         }
-    
+
+        /// <summary>
+        /// Creates a pool of type <typeparamref name="TComponent"/> and returns itself.
+        /// </summary>
+        /// <param name="numberMaxComponents">Specified components' capacity for the pool if it needs to be created.</param>
+        /// <param name="isTag">Is <typeparamref name="TComponent"/> a tag</param>
+        public Pools Add<TComponent>(int numberMaxComponents = 0, bool isTag = false) where TComponent : struct
+        {
+            Create<TComponent>(numberMaxComponents, isTag);
+            return this;
+        }
+
         /// <summary>
         /// Returns the pool of type <typeparamref name="TComponent"/> and creates it if needed.
         /// </summary>
         /// <param name="numberMaxComponents">Specified components' capacity for the pool if it needs to be created.</param>
+        /// <param name="isTag">Is <typeparamref name="TComponent"/> a tag</param>
         public Pool<TComponent> Get<TComponent>(int numberMaxComponents = 0, bool isTag = false) where TComponent : struct
         {
             for (var i = 0; i < _poolCount; ++i)
                 if (typeof(TComponent) == _pools[i].GetComponentType())
                     return (Pool<TComponent>)_pools[i];
-            Add<TComponent>(numberMaxComponents, isTag);
-            return (Pool<TComponent>)_pools[_poolCount - 1];
+            return (Pool<TComponent>)Create<TComponent>(numberMaxComponents, isTag);
         }
 
-        private void Add<TComponent>(int numberMaxComponents = 0, bool isTag = false) where TComponent : struct
+        private IPool Create<TComponent>(int numberMaxComponents, bool isTag) where TComponent : struct
         {
             System.Array.Resize(ref _pools, _poolCount + 1);
             numberMaxComponents = numberMaxComponents < 1 ? _config.NumberMaxComponents : numberMaxComponents;
             var config = new Config.Pools(_config.NumberMaxEntities, numberMaxComponents);
-            _pools[_poolCount++] = isTag ? new TagPool<TComponent>(_entities, config) : new Pool<TComponent>(_entities, config);
+            return _pools[_poolCount++] = isTag ? new TagPool<TComponent>(_entities, config) : new Pool<TComponent>(_entities, config);
         }
     }
 }
